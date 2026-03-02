cee8303208aa84d570a83bcf9794e85ef8350da6
박창조 <ckdwhdev@gmail.com>
2026-03-02 15:20:15 +0900

    docs: architecture.md 수정

--- src/auth.ts
+++ src/auth.ts
@@ -3,10 +3,13 @@ import { nextCookies } from "better-auth/next-js";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
+  // Supabase 연동
   database: new Pool({
     connectionString: process.env.DATABASE_URL!,
     ssl: { rejectUnauthorized: false },
   }),
+
+  // 소셜 로그인 연동
   socialProviders: {
     google: {
       clientId: process.env.AUTH_GOOGLE_ID!,
